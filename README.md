f4df816006b252bfa31d09fc60525ef46dbaf11d
ARIS <aris.sunandar@salt.co.id>
2024-12-19 18:54:11 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+
+# Perhatikan!
+- Sebelum menjalankan script ini, pastikan Anda memiliki Node.js dan Lighthouse terinstal di sistem Anda.
+- Untuk menginstal Lighthouse, Anda dapat menggunakan perintah:
+```bash
+npm install --global lighthouse
+```
+
+# Penggunaan
+- Untuk menjalankan script, Anda dapat menggunakan perintah:
+```bash
+./run.sh
+```
+
+# Catatan
+- Jika muncul error seperti "Permission denied", pastikan Anda telah menjalankan script berikut:
+```bash
+chmod +x run.sh
+```
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Konfigurasi default
+HEADLESS="--chrome-flags='--headless'"
+USERAGENT='--emulatedUserAgent="Chrome-Lighthouse"'
+
+# Meminta input dari pengguna
+read -p "Enter the URL to test: " URL
+read -p "Enter the number of iterations: " ITERATIONS
+read -p "Use Chrome Headless? (Y/n): " IS_HEADLESS
+read -p "Use emulated user agent? (Y/n): " IS_USER_AGENT
+
+# Memeriksa apakah URL dan ITERATIONS tidak kosong
+if [ -z "$URL" ] || [ -z "$ITERATIONS" ]; then
+    URL="https://www.telkomsel.com/"
+    ITERATIONS=1
+fi
+
+# Memeriksa apakah IS_HEADLESS adalah "n" atau "N"
+if [ "$IS_HEADLESS" = "n" ] || [ "$IS_HEADLESS" = "N" ]; then
+    HEADLESS=""
+fi
+
+# Memeriksa apakah IS_USER_AGENT adalah "n" atau "N"
+if [ "$IS_USER_AGENT" = "N" ] || [ "$IS_USER_AGENT" = "n" ]; then
+    USERAGENT=""
+fi
+
+# Membuat folder laporan
+mkdir -p ./reports
+cd ./reports
+
+# Loop untuk menjalankan Lighthouse
+for ((i=1; i<=ITERATIONS; i++)); do
+    echo "Running Lighthouse Test #$i on $URL..."
+    lighthouse "$URL" --form-factor=desktop --preset=desktop --view=true $HEADLESS $USERAGENT
+done
+
+echo "All tests completed."
