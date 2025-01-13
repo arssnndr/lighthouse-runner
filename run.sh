b4a50c436b050c153c6a6c260ff09355e0c1de43
ARIS <aris.sunandar@salt.co.id>
2025-01-13 13:54:37 +0700

    add device choice

--- run.sh
+++ run.sh
@@ -8,18 +8,24 @@ if ! command -v lighthouse &> /dev/null; then
 fi
 
 # Konfigurasi default
+DEVICE="--screenEmulation.disabled --form-factor=desktop"
 HEADLESS="--chrome-flags='--headless'"
 USERAGENT='--emulatedUserAgent="Chrome-Lighthouse"'
 
 # Meminta input dari pengguna
 read -p "Enter the URL to test: " URL
+read -p "Use Desktop Device? (Y/n): " IS_DESKTOP
 read -p "Enter the number of iterations: " ITERATIONS
 read -p "Use Chrome Headless? (Y/n): " IS_HEADLESS
 read -p "Use emulated user agent? (Y/n): " IS_USER_AGENT
 
+# Memeriksa apakah IS_DESKTOP adalah "n" atau "N"
+if [ "$IS_DESKTOP" = "n" ] || [ "$IS_DESKTOP" = "N" ]; then
+    DEVICE="--form-factor=mobile --screenEmulation.mobile"
+fi
+
 # Memeriksa apakah URL dan ITERATIONS tidak kosong
-if [ -z "$URL" ] || [ -z "$ITERATIONS" ]; then
-    URL="https://www.telkomsel.com/"
+if [ -z "$ITERATIONS" ]; then
     ITERATIONS=1
 fi
 
@@ -40,7 +46,7 @@ cd ./reports
 # Loop untuk menjalankan Lighthouse
 for ((i=1; i<=ITERATIONS; i++)); do
     echo "Running Lighthouse Test #$i on $URL..."
-    lighthouse "$URL" --form-factor=desktop --preset=desktop --view=true $HEADLESS $USERAGENT
+    lighthouse "$URL" --view=true $HEADLESS $USERAGENT $DEVICE
 done
 
 echo "All tests completed."
